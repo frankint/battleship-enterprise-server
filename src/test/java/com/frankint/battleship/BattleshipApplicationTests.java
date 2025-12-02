e4ecd47cc62cf0739ae6b83d1ba1f1b875158da0
frankint <160731641+frankint@users.noreply.github.com>
2025-12-02 16:48:11 +0100

    Chore: Initialize Project Skeleton (#4)
    * chore: initialize spring boot project with dependencies

---
+++ src/test/java/com/frankint/battleship/BattleshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frankint.battleship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
